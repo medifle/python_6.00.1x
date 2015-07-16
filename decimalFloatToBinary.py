3d144d303f15acf7fde65041b6ce388fb9077404
medifle <medifle@gmail.com>
2015-07-16 14:45:41 +0800

    rename and add comment

--- decimalFloatToBinary.py
+++ decimalFloatToBinary.py
@@ -1,6 +1,7 @@
 # Lec 3.4, slide 4
 # convert decimal float to binary
 
+# Ask to input a decimal float number
 a = float(raw_input('Enter a decimal number:'))
 
 if a < 0:
@@ -10,10 +11,14 @@ else:
     ifNeg = False
 
 p = 1
+# Convert the input float into integer by multiplying 2 iteratively
+# As long as the input float remains a fraction, add p by 1
 while (a * (2 ** p)) % 1 != 0:
     p += 1
+    
 b = int(a * (2 ** p))
 
+# The following block converts decimal int to binary number
 result = ''
 if a == 0:
     result = 0.0
@@ -21,7 +26,9 @@ else:
     while b > 0:
         result = str(b % 2) + result
         b = b / 2
-    result = float(result) / (10 ** p) # precision problem
+        
+    # Move dot to proper place
+    result = float(result) / (10 ** p)
 
 if ifNeg:
     result = -result
