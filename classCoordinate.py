9cba4d8ccd93471230ee8ddb8f4e976bb8bbb2fa
medifle <medifle@gmail.com>
2015-07-28 09:51:05 +0800

    add __str__ and distance methods

--- classCoordinate.py
+++ classCoordinate.py
@@ -1,3 +1,12 @@
+import math
+
 class Coordinate(object):
     def __init__(self, x=0, y=0):
-        self = x
+        self.x = x
+        self.y = y
+        
+    def __str__(self):
+        return "<" + str(self.x) + "," + str(self.y) + ">"
+        
+    def distance(self, other):
+        return math.sqrt(pow((self.x - other.x), 2) + pow((self.y - other.y), 2))
