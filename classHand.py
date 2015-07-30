3f5ab10e33ad661376be56c3da6aad3e2f1588ef
medifle <medifle@gmail.com>
2015-07-30 13:23:55 +0800

    add alternative solution for update()

--- classHand.py
+++ classHand.py
@@ -107,3 +107,23 @@ class Hand(object):
 
 # myHand.update('za')
 # print myHand
+
+# --Alternative solution for update() START--
+        # Make a copy of the hand, and try to update it
+        new_hand = self.hand.copy()
+        for letter in word:
+            try:
+                new_hand[letter] -= 1
+            except KeyError:
+                # if 'letter' isn't in the hand, we can't make the word from this hand.
+                return False
+        for letter in new_hand.keys():
+            # If any of the letter counts of the new hand are less than zero after the
+            # update, then we can't make the word from this hand.
+            if new_hand[letter] < 0:
+                return False
+        # If we've gotten to here, we must be able to make the word from this hand.
+        # Set self.hand to the new, updated hand and return True.
+        self.hand = new_hand
+        return True
+# --Alternative solution for update() END--
