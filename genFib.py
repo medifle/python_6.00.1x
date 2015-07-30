dbeb1a58e093536a004eb6a4959fea5a5c24b158
medifle <medifle@gmail.com>
2015-07-30 15:25:32 +0800

    add genFib, a generator instance

---
+++ genFib.py
@@ -0,0 +1,14 @@
+def genFib():
+    fibn_1 = 1 #fib(n - 1)
+    fibn_2 = 0 #fib(n - 2)
+    while True:
+        # fib(n) = fib(n - 1) + fib(n - 2)
+        next = fibn_1 + fibn_2
+        yield next
+        fibn_2 = fibn_1
+        fibn_1 = next
+        
+# a = genFib()
+# a.next()
+# a.next()
+# a.next()
