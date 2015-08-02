659d61177f01861457e845b0c0f73c50ec518ecd
medifle <medifle@gmail.com>
2015-08-02 15:46:18 +0800

    add depth first binary search

---
+++ treeSearch.py
@@ -0,0 +1,73 @@
+### first version is just a binary tree
+
+class binaryTree(object):
+    def __init__(self, value):
+        self.value = value
+        self.leftBranch = None
+        self.rightBranch = None
+        self.parent = None 
+    def setLeftBranch(self, node):
+        self.leftBranch = node
+    def setRightBranch(self, node):
+        self.rightBranch = node
+    def setParent(self, parent):
+        self.parent = parent
+    def getValue(self):
+        return self.value
+    def getLeftBranch(self):
+        return self.leftBranch
+    def getRightBranch(self):
+        return self.rightBranch
+    def getParent(self):
+        return self.parent
+    def __str__(self):
+        return self.value
+
+# Depth First binary Search
+def DFSBinary(root, fcn, n):
+    queue = [root]
+    while len(queue) > 0:
+        print 'at node ' + str(queue[0].getValue())
+        if fcn(queue[0], n):
+            return True
+        else:
+            temp = queue.pop(0)
+            if temp.getRightBranch():
+                queue.insert(0, temp.getRightBranch())
+            if temp.getLeftBranch():
+                queue.insert(0, temp.getLeftBranch())
+    return False
+
+# create a 8 elements of tree
+n5 = binaryTree(5)
+n2 = binaryTree(2)
+n1 = binaryTree(1)
+n4 = binaryTree(4)
+n8 = binaryTree(8)
+n6 = binaryTree(6)
+n7 = binaryTree(7)
+n3 = binaryTree(3)
+
+# construct tree
+# there are (8 - 1) edges(or branches), (8 - 1) * 2 lines to construct.
+n5.setLeftBranch(n2)
+n2.setParent(n5)
+n5.setRightBranch(n8)
+n8.setParent(n5)
+n2.setLeftBranch(n1)
+n1.setParent(n2)
+n2.setRightBranch(n4)
+n4.setParent(n2)
+n8.setLeftBranch(n6)
+n6.setParent(n8)
+n6.setRightBranch(n7)
+n7.setParent(n6)
+n4.setLeftBranch(n3)
+n3.setParent(n4)
+
+# define fcn
+def find(node, n):
+    '''n is an integer
+    node is an instance of binaryTree class
+    Returns boolean, to test if the value of node equals to n'''
+    return node.getValue() == n
